1747f5b2cdb658c58e119bb02c02d86ca7a9b276
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2023-09-19 14:36:30 -0300

    Create 0_string_count.py

---
+++ 0_string_count.py
@@ -0,0 +1,8 @@
+#!/bin/python2.7
+
+# Neste programa vamos executar alguns métodos de string
+
+nome=input(" Digite seu nome: ")
+letra=input("Qual letra gostaria de contar? ")
+print( nome.count(letra) )
+print("Olá," , nome , " Seja bem vindo. ")
