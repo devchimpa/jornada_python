86c39c7141310899e5217df2c3274890f1bf6947
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2023-09-19 14:26:11 -0300

    Update iniciando_strings.py

--- iniciando_strings.py
+++ iniciando_strings.py
@@ -1,1 +1,8 @@
+#!/bin/python3
 
+#Programa para capturar entrada e salvar em variável, após isso usar o método captalize.
+
+entrada=input(" Digite seu nome: ")
+nome=entrada.capitalize()
+
+print("Olá," , nome , " Seja bem vindo. ")
