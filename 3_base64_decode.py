0c088d6ad20aa580e69d61223acaa49adf87b143
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2023-09-22 18:33:18 -0300

    Create 3_base64_decode.py

---
+++ 3_base64_decode.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+import base64
+
+print(" **** Gerador de senhas Aleatórias ****")
+#frase =input( "Insira a senha para codificar: " )
+
+frase=input("insira o texto: ")
+frase_codificada = base64.b64encode(frase.encode("utf-8"))
+frase_nova=str(frase_codificada)
+
+letras=(len(frase_codificada))
+print("Esta senha codificada fica: ", frase_nova[2:14:])
+#print(frase_nova)
