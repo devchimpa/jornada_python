b461ad225ebcae925f021594a2e791c11f04d735
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2023-10-03 18:27:15 -0300

    Create 9_tuplas.py

---
+++ 9_tuplas.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+
+minha_tupla=("Rodrigo" ,"Marcella", "Salém" , "Olaf")
+print(minha_tupla)
+print(minha_tupla[0])
+print(minha_tupla.count("Marcella"))
+print(minha_tupla.index("Salém"))
