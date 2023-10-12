41ff0f11d8f9055287a9052dfa7c80588fde73b3
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2023-10-11 22:01:31 -0300

    Create 10_dicionarios.py

---
+++ 10_dicionarios.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+#dicionarios em python
+
+zoo={"Mamiferos": ["Urso Panda", "Coala", "Leão",] , "repteis": ["Tartaruga" , "Jacaré" , "Jibóia",]}
+
+print(zoo.keys())
+#zoo.items()
+#print(zoo["Mamiferos"])
+
+#dicionarios em python
+
+zoo={"Mamiferos": ["Urso Panda", "Coala", "Leão",] , "repteis": ["Tartaruga" , "Jacaré" , "Jibóia",]}
+
+print(zoo.keys())
+#zoo.items()
+#print(zoo["Mamiferos"])
