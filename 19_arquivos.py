befb3e43765e1be92b5e72f74a6045a7e87708fb
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2023-10-12 11:43:41 -0300

    Create 19_arquivos.py

---
+++ 19_arquivos.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+
+arquivo=open ("/home/chimpa/Documents/ArquivoPython.txt" , "w" )
+
+arquivo.write(" \n Criando um arquivo, mas será essa a melhor maneira? \n " )
+
+arquivo.close()
