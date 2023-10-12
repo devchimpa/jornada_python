991ef616041da5e80511ca76d2f5cac961aab955
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2023-10-12 10:34:12 -0300

    Update 18_try_except.py

--- 18_try_except.py
+++ 18_try_except.py
@@ -1,7 +1,5 @@
 #!/bin/python3
 
-mamiferos=[ "Gato" , "Ca#!/bin/python3
-
 mamiferos=[ "Gato" , "Cachorro" , "Macaco" ]
 
 print(" De 0 a 2, selecione um número.")
@@ -12,14 +10,5 @@ try:
     print(mamiferos[selecione])
 
 except:
-    print("Algum erro ocorreu. Por favor, verifique o número selecionado.")chorro" , "Macaco" ]
-
-print(" De 0 a 2, selecione um número.")
-
-selecione=int(input("Qual item gostaria de ver? "))
-
-try:
-    print(mamiferos[selecione])
-
-except:
     print("Algum erro ocorreu. Por favor, verifique o número selecionado.")
+
