41a07fa532f4cac4717f4eb200e3f9df628bb349
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2023-10-11 22:05:08 -0300

    Create 15_function.py

---
+++ 15_function.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+#dicionarios em python
+
+print("Criando e chamando funções.")
+
+print(" Uma função deve ser declarada com \"def\" ")
+
+print(" \"def\" nome_da_função(): " )
+
+
+
+def soma_valor():
+	valor_um=int(input("digite o valor um: "))
+	valor_dois=int(input("digite o valor dois: "))
+	resultado=(valor_um + valor_dois)
+	print(resultado)
+soma_valor()
