3e7029b43995481e32eeda48abcbbb6c2acacf58
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2024-08-19 17:35:14 -0300

    Create template.py

---
+++ template.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+####################################
+# -*- coding UTF-8 -*-
+# Nome do programa: Nome do programa.
+# Autor: Rodrigo Pinheiro
+# Data: 28/06/2024
+####################################
+#
+#
